3853c9d30d2a012882bac6b92212285941a41750
MikhailWahib <mikhailwahib1@gmail.com>
2024-03-22 02:13:42 +0200

    Init commit

---
+++ src/main/java/com/example/studentsmanagerapi/StudentsManagerApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.studentsmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsManagerApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentsmanagerapi/StudentsManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
